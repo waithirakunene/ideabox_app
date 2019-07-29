a7b753397a2a4098feb5eb1020217455041e9e8f
waithirakunene <waithira.kunene@gmail.com>
2019-07-29 19:47:33 +0300

    IdeaBox app

--- ideabox_app/urls.py
+++ ideabox_app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
@@ -27,4 +29,5 @@ urlpatterns = [
     path('', include('ideabox.urls'))
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
