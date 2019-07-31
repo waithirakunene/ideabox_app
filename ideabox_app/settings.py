ffbfd79dc3ac92c35feaa2a70677f5adede3f2b3
waithirakunene <waithira.kunene@gmail.com>
2019-07-31 23:55:57 +0300

    deploying to heroku

--- ideabox_app/settings.py
+++ ideabox_app/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=1us@a48+vi1g9bf*(z&spy#@lq+r=113!_i-)o(#+8yk760u&'
+#SECRET_KEY = '=1us@a48+vi1g9bf*(z&spy#@lq+r=113!_i-)o(#+8yk760u&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-ideabox.herokuapp.com']
 
 
 # Application definition
